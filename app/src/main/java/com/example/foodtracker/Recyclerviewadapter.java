0fedca1dae0a6cbf9e7974319a17918e530959d2
engincanoz <ozengincan424@gmail.com>
2023-12-14 23:11:48 +0300

    improvements

--- app/src/main/java/com/example/foodtracker/Recyclerviewadapter.java
+++ app/src/main/java/com/example/foodtracker/Recyclerviewadapter.java
@@ -10,42 +10,42 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
-//public class Recyclerviewadapter extends RecyclerView.Adapter<Recyclerviewadapter.MyViewHolder> {
-    //Context context;
-    //ArrayList<ProductModel> list;
-    //public  Recyclerviewadapter(Context context, ArrayList<ProductModel> list) {
-        //this.list = list;
-       // this.context = context;
-    //}
+public class Recyclerviewadapter extends RecyclerView.Adapter<Recyclerviewadapter.MyViewHolder> {
+    Context context;
+    ArrayList<ProductModel> list;
+    public  Recyclerviewadapter(Context context, ArrayList<ProductModel> list) {
+        this.list = list;
+        this.context = context;
+    }
 
-  // public Recyclerviewadapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-    //    LayoutInflater inflater = LayoutInflater.from(context);
-        //View view = inflater.inflate(R.layout.recyclerviewrow,parent,false);
-        //return new Recyclerviewadapter.MyViewHolder(view);
-    //}
+    public Recyclerviewadapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.recyclerviewrow,parent,false);
+        return new Recyclerviewadapter.MyViewHolder(view);
+    }
 
-   // @Override
-    //public void onBindViewHolder(@NonNull Recyclerviewadapter.MyViewHolder holder, int position) {
-      //  holder.name.setText(list.get(position).getProductName());
-        //holder.freshness.setText(list.get(position).getFreshness());
-        //holder.expiratondate.setText(list.get(position).getExpirationdate());
-    //}
+    @Override
+    public void onBindViewHolder(@NonNull Recyclerviewadapter.MyViewHolder holder, int position) {
+        holder.name.setText(list.get(position).getProductName());
+        holder.freshness.setText(list.get(position).getFreshness());
+        holder.expiratondate.setText(list.get(position).getExpirationdate());
+    }
 
-    //@Override
-    //public int getItemCount() {
-      //  return list.size();
-    //}
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
 
-   // public static class MyViewHolder extends RecyclerView.ViewHolder {
-     //   TextView freshness;
-       // TextView expiratondate;
-        //TextView name;
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView freshness;
+        TextView expiratondate;
+        TextView name;
 
-       // public MyViewHolder(@NonNull View itemView) {
-         //   super(itemView);
-            //freshness = itemView.findViewById(R.id.Freshness);
-          //  expiratondate = itemView.findViewById(R.id.textView3);
-            //name = itemView.findViewById(R.id.textView13);
-       // }
-    //}
-//}
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            freshness = itemView.findViewById(R.id.Freshness);
+            expiratondate = itemView.findViewById(R.id.textView3);
+            name = itemView.findViewById(R.id.textView13);
+        }
+    }
+}
