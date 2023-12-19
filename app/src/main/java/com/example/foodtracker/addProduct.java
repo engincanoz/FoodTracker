2eafd250b93570a5b3748f84a7b5c24a73366a80
engincanoz <ozengincan424@gmail.com>
2023-12-20 00:35:12 +0300

    fix

--- app/src/main/java/com/example/foodtracker/addProduct.java
+++ app/src/main/java/com/example/foodtracker/addProduct.java
@@ -65,7 +65,6 @@ public class addProduct extends AppCompatActivity {
                 Log.d(tag, target.toString());
                 Log.d(tag, ingredientsList.toString());
 
-                // Parse the date string and obtain java.sql.Date
 
                 if (dateText != null) {
                     if (checkContains(target)) {
